33b78a49d63402648aad32f3f6b7df2713b240a7
wuttashi1 <den.velikiy562381@gmail.com>
2026-07-07 21:28:55 +0200

    Update portfolio design

---
+++ git.bat
@@ -0,0 +1,7 @@
+echo "# WUTSHYsite" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/wuttashi1/WUTSHYsite.git
+git push -u origin main
